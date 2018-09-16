e20d5d33c496ddf54985d4c8c2f0437d7310938a
Matthias Gekiere <matthias.gekiere@gmail.com>
2018-09-16 15:09:14 +0200

    menu component

---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent {
+  faSearchIcon = faSearch;
+}
