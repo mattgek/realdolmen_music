dbdb72fbb2e37525c7957340f32ceff78ff22e31
Matthias Gekiere <matthias.gekiere@gmail.com>
2018-09-16 15:08:13 +0200

    components & pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
+import { AlbumCardModule, MenuModule } from './components';
+import { HomeModule } from './pages';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  bootstrap: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MenuModule, HomeModule, AlbumCardModule],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pages/library/components/liked-songs/liked-songs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-liked-songs',
+  templateUrl: './liked-songs.component.html',
+  styleUrls: ['./liked-songs.component.scss']
+})
+export class LikedSongsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
