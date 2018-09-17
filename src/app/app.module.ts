9429b4df9dee85540d06f98af1efa6605cd2f508
Matthias Gekiere <matthias.gekiere@gmail.com>
2018-09-17 18:56:41 +0200

    menu fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
+import { MenuModule } from './components/menu';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, MenuModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
