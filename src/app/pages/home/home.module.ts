04b7acfaf21bf7410220acf1b56b570f79bfb87a
Matthias Gekiere <matthias.gekiere@gmail.com>
2018-09-17 22:48:36 +0200

    album-card component
    code in home.component.html needs to be removed after reviez is correct. this is for dev purposes only album-card needs to be in a carousel/scroller

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { AlbumCardModule } from '../../components';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home.routing';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, AlbumCardModule],
   exports: [HomeComponent]
 })
 export class HomeModule {}
