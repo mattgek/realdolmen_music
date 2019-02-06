6fa6ac59e0608253fc8e4e5503737b17d0d08861
Matthias Gekiere <matthias.gekiere@gmail.com>
2019-02-06 23:12:59 +0100

    add overlay compnent version cdk

--- src/app/overlay/overlay.module.ts
+++ src/app/overlay/overlay.module.ts
@@ -1,10 +1,14 @@
+import { OverlayModule as cdkOverlay } from '@angular/cdk/overlay';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { OverlayComponent } from './overlay.component';
+import { OverlayDirective } from './overlay.directive';
+import { OverlayService } from './overlay.service';
 
 @NgModule({
-  declarations: [OverlayComponent],
-  imports: [CommonModule],
-  exports: [OverlayComponent]
+  declarations: [OverlayComponent, OverlayDirective],
+  imports: [CommonModule, cdkOverlay],
+  exports: [OverlayComponent, OverlayDirective],
+  providers: [OverlayService]
 })
 export class OverLayModule {}
