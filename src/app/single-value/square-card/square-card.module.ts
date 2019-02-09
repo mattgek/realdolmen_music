bc146db9dd8e7958e4125ea6f58605d933978291
Matthias Gekiere <matthias.gekiere@gmail.com>
2019-02-09 11:40:28 +0100

    cleanup code

--- src/app/single-value/square-card/square-card.module.ts
+++ src/app/single-value/square-card/square-card.module.ts
@@ -1,13 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { OverLayModule } from 'src/app/overlay';
-import { OverlayComponent } from 'src/app/overlay/overlay.component';
 import { SquareCardComponent } from './square-card.component';
 
 @NgModule({
   declarations: [SquareCardComponent],
   imports: [CommonModule, OverLayModule],
-  exports: [SquareCardComponent],
-  entryComponents: [OverlayComponent]
+  exports: [SquareCardComponent]
 })
 export class SquareCardModule {}
